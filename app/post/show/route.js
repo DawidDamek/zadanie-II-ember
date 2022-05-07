7816765182e99994405645dd3b5a2e88e04e1ed1
dawiddamek <dawid.damek98@gmail.com>
2022-05-07 15:25:20 +0200

    add view of single post

--- app/post/show/route.js
+++ app/post/show/route.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class PostShowRoute extends Route {}
+export default class PostShowRoute extends Route {
+  @service store;
+
+  async model(params) {
+    const user = await this.store.findRecord('post', params.id);
+    return user;
+  }
+}
