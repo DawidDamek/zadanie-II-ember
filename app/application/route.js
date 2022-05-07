323d26065b61a61cd71f180a15310950c7a3ff83
dawiddamek <dawid.damek98@gmail.com>
2022-05-07 13:44:01 +0200

    add realtion of post and user

--- app/application/route.js
+++ app/application/route.js
@@ -6,6 +6,7 @@ export default class ApplicationRoute extends Route {
 
   async beforeModel() {
     window.localStorage.clear();
+
     const user1 = {
       username: 'admin',
       password: 'admin123',
@@ -17,19 +18,31 @@ export default class ApplicationRoute extends Route {
       password: 'user123',
       email: 'user@user.com',
     };
-
-    const post1 = { title: 'Tytuł testowy 1', body: 'Zawartość testowa 1' };
-    const post2 = { title: 'Tytuł testowy 2', body: 'Zawartość testowa 2' };
-    const post3 = { title: 'Tytuł testowy 3', body: 'Zawartość testowa 3' };
-
     const user1Model = this.store.createRecord('user', user1);
     const user2Model = this.store.createRecord('user', user2);
+    await user1Model.save();
+    await user2Model.save();
+
+    const post1 = {
+      title: 'Tytuł testowy 1',
+      body: 'Zawartość testowa 1',
+      owner: user1Model,
+    };
+    const post2 = {
+      title: 'Tytuł testowy 2',
+      body: 'Zawartość testowa 2',
+      owner: user1Model,
+    };
+    const post3 = {
+      title: 'Tytuł testowy 3',
+      body: 'Zawartość testowa 3',
+      owner: user2Model,
+    };
+
     const post1Model = this.store.createRecord('post', post1);
     const post2Model = this.store.createRecord('post', post2);
     const post3Model = this.store.createRecord('post', post3);
 
-    await user1Model.save();
-    await user2Model.save();
     await post1Model.save();
     await post2Model.save();
     await post3Model.save();
