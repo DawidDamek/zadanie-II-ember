fb8f640fa9fc9cbff8f6d343d3cd8bbfafc98abf
dawiddamek <dawid.damek98@gmail.com>
2022-05-14 14:25:10 +0200

    register new user

--- app/register/route.js
+++ app/register/route.js
@@ -1,3 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { storageFor } from 'ember-local-storage';
+import { action } from '@ember/object';
 
-export default class RegisterRoute extends Route {}
+export default class RegisterRoute extends Route {
+  @storageFor('logged-as') loggedAs;
+  @service store;
+  @service router;
+
+  beforeModel() {
+    const userId = this.loggedAs.get('id');
+    if (userId) {
+      this.router.transitionTo('home');
+      return;
+    }
+  }
+
+  model() {
+    return this.store.createRecord('user');
+  }
+}
