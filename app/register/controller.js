3a6ad26b4c90fad4a817612ac0818e04e93cca8f
Dawid Damek <dawid.damek98@gmail.com>
2022-06-01 21:22:39 +0200

    add session service

--- app/register/controller.js
+++ app/register/controller.js
@@ -1,10 +1,8 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { storageFor } from 'ember-local-storage';
+import { action } from '@ember/object';
 
 export default class RegisterController extends Controller {
-  @storageFor('logged-as') loggedAs;
   @service store;
   @service router;
 
@@ -12,27 +10,22 @@ export default class RegisterController extends Controller {
   onLoginChange(event) {
     this.model.username = event.target.value;
   }
-
   @action
   onEmailChange(event) {
     this.model.email = event.target.value;
   }
-
   @action
   onPhotoURLChange(event) {
     this.model.photoURL = event.target.value;
   }
-
   @action
   onPasswordChange(event) {
     this.model.password = event.target.value;
   }
-
   @action
   async onSubmit(event) {
     event.preventDefault();
     await this.model.save();
-    const user = this.model.id;
     this.router.transitionTo('login');
   }
 }
--- app/register/route.js
+++ app/register/route.js
@@ -1,21 +1,19 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { storageFor } from 'ember-local-storage';
-import { action } from '@ember/object';
 
 export default class RegisterRoute extends Route {
-  @storageFor('logged-as') loggedAs;
-  @service store;
   @service router;
+  @service store;
+  @service session;
 
   beforeModel() {
-    const userId = this.loggedAs.get('id');
-    if (userId) {
+    const { isUserLoggedIn } = this.session;
+
+    if (isUserLoggedIn) {
       this.router.transitionTo('home');
       return;
     }
   }
-
   model() {
     return this.store.createRecord('user');
   }
