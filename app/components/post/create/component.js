870d201f7ef24d993223ad80d2855b8261fc5d90
dawiddamek <dawid.damek98@gmail.com>
2022-06-04 15:45:46 +0200

    add create post feature

--- app/components/post/create/component.js
+++ app/components/post/create/component.js
@@ -1,9 +1,18 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class CreateodalComponent extends Component {
+  @service store;
+  @service session;
   @tracked isShowCreateModal = false;
+  @tracked title = '';
+  @tracked content = '';
+
+  get hasEmptyFields() {
+    return !(this.title && this.content);
+  }
 
   @action
   onShowModal() {
@@ -15,5 +24,36 @@ export default class CreateodalComponent extends Component {
   onHideModal() {
     console.log('hide');
     this.isShowCreateModal = false;
+    this.clearFields();
+  }
+
+  @action
+  onTitleChange(event) {
+    this.title = event.target.value;
+    console.log(this.title);
+  }
+
+  @action
+  onContentChange(event) {
+    this.content = event.target.value;
+    console.log(this.content);
+  }
+
+  @action
+  async onSave() {
+    const post = {
+      owner: this.session.currentUser,
+      title: this.title,
+      body: this.body,
+    };
+    const postModel = this.store.createRecord('post', post);
+    await postModel.save();
+    this.clearFields();
+    this.isShowCreateModal = false;
+  }
+
+  clearFields() {
+    this.title = '';
+    this.content = '';
   }
 }
