39ae60a9825d030e0821a095e7f9df7735055255
Dawid Damek <dawid.damek98@gmail.com>
2022-07-22 21:34:35 +0200

    add auth0 login

--- app/home/users/route.js
+++ app/home/users/route.js
@@ -4,7 +4,12 @@ import { inject as service } from '@ember/service';
 export default class UsersRoute extends Route {
   @service store;
 
-  model() {
+  async model() {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
+    });
     const users = this.store.findAll('user');
     return users;
   }
